3a7351be5f2556a674ad95c0b54236fdc8cf5867
Fiji404 <radek42310o@gmail.com>
2022-04-10 20:07:38 +0200

    fixes

--- dist/index.c719088e.js
+++ dist/index.c719088e.js
@@ -4,15 +4,14 @@ const menuHeaders = document.querySelectorAll('.aside-nav__header-btn');
 const hiddenMenu = document.querySelector('.aside-nav__list_expand');
 const workspaceControls = document.querySelectorAll('.code-workspace__controls');
 const codeBlocks = document.querySelectorAll('.code-workspace__list-item');
-
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', ()=>{
     menuNav.classList.toggle('active');
 });
-
-menuHeaders.forEach((element) => {
-    element.addEventListener('click', () => {
+menuHeaders.forEach((element)=>{
+    element.addEventListener('click', ()=>{
         element.classList.toggle('active');
         element.nextElementSibling.classList.toggle('show');
     });
 });
 
+//# sourceMappingURL=index.c719088e.js.map
